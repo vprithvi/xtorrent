4c8f64e9d4ab8b41e60694e304b34baa2aecf350
Valampuri Lakshminarayanan <coolvalam@gmail.com>
2011-11-18 19:38:33 -0500

    Added getPeerInfo() method.

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -1,4 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 
 public class peerProcess {
-
+	String pi;
+	ArrayList<String> peerInfo = new ArrayList<String>();
+	
+	public void getPeerInfo() {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("PeerInfo.cfg"));
+			while((pi = in.readLine()) != null) {
+				 String[] tokens = pi.split("\\s+");
+			     peerInfo.add(tokens[0]);
+			     
+				 //peerInfoVector.addElement(new RemotePeerInfo(tokens[0], tokens[1], tokens[2]));
+			
+			}
+			
+			in.close();
+		}
+		catch (Exception ex) {
+			System.out.println(ex.toString());
+		}
+	}
 }
