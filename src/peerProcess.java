3b19c7ea02ee65ff459097c7c4c902719a858f72
Valampuri Lakshminarayanan <coolvalam@gmail.com>
2011-11-23 20:42:37 -0500

    Refactored. Server port is now opened on myPort.

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -11,6 +11,8 @@ import java.util.Vector;
 public class peerProcess extends Thread {
 
 	public static String myID;
+	public static int myPort;
+	
 	public Vector<RemotePeerInfo> myPeerInfo;
 	static int haveFile;
 	static int nofPreferredNeighbour;
@@ -20,40 +22,43 @@ public class peerProcess extends Thread {
 	static int fileSize;
 	static int pieceSize;
 	static int nofPieces;
-	
+
 	private ServerSocket server;
+	
 	//Method to read PeerInfo.cfg
 	public void getPeerInfo() {
-			String st;
-			myPeerInfo = new Vector<RemotePeerInfo>();
-			try {
-				BufferedReader in = new BufferedReader(new FileReader("PeerInfo.cfg"));
-				while((st = in.readLine()) != null) {
-					
-					 String[] tokens = st.split("\\s+");
-					 myPeerInfo.addElement(new RemotePeerInfo(tokens[0], tokens[1], tokens[2]));
-					 
-					 if(myID.equals(tokens[0])){
-						 haveFile = Integer.parseInt(tokens[3]);
-					 }
+		String st;
+		myPeerInfo = new Vector<RemotePeerInfo>();
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("PeerInfo.cfg"));
+			while((st = in.readLine()) != null) {
+
+				String[] tokens = st.split("\\s+");
+				myPeerInfo.addElement(new RemotePeerInfo(tokens[0], tokens[1], tokens[2]));
+
+				if(myID.equals(tokens[0])){
+					haveFile = Integer.parseInt(tokens[3]);
+					myPort = Integer.parseInt(tokens[2]);
 				}
-				
-				in.close();
 			}
-			catch (Exception ex) {
-				System.out.println(ex.toString());
-			}
+
+			in.close();
+		}
+		catch (Exception ex) {
+			System.out.println(ex.toString());
+		}
 	}
-	
+
+	//Method to read CommonInfo.cfg
 	public void getCommonConfig(){
 		String cfg;
 		ArrayList<String> configInfo = new ArrayList<String>();
 		try {
 			BufferedReader in = new BufferedReader(new FileReader("Common.cfg"));
 			while((cfg = in.readLine()) != null) {
-				
-				 String[] tokens = cfg.split("\\s+");
-				 configInfo.add(tokens[1]);
+
+				String[] tokens = cfg.split("\\s+");
+				configInfo.add(tokens[1]);
 			}
 			nofPreferredNeighbour = Integer.parseInt(configInfo.get(0));
 			unchokingInterval = Integer.parseInt(configInfo.get(1));
@@ -62,28 +67,49 @@ public class peerProcess extends Thread {
 			fileSize = Integer.parseInt(configInfo.get(4));
 			pieceSize = Integer.parseInt(configInfo.get(5));
 			nofPieces = (int)Math.ceil((double)fileSize/pieceSize);
-					
+
 			in.close();
 		}
 		catch (Exception ex) {
 			System.out.println(ex.toString());
 		}		
-		
+
 	}
 	
+	//Constructor
+	public peerProcess() throws Exception {
+		server = new ServerSocket(myPort);
+		System.out.println("Server listening on port " + myPort);
+		this.start();
+	}
+
+	//Overriding run method
+	public void run() {
+		while(true) {
+			try {
+				System.out.println("Waiting for connections.");
+				Socket client = server.accept();
+				System.out.println("Accepted a connection from: "+
+						client.getInetAddress());
+				Connect c = new Connect(client);
+			} catch(Exception e) {}
+		}
+	}
+	
+	//Main method
 	public static void main(String[] args)  throws Exception {
 		peerProcess p = new peerProcess();
 		p.myID = args[0];
 		p.getPeerInfo();
 		p.getCommonConfig();
-		
-		
+
+
 		/* test to print parameters
 		 * 
 		for(int x=0;x<p.myPeerInfo.size();x++) {
 			RemotePeerInfo peer = (RemotePeerInfo)p.myPeerInfo.elementAt(x);
 			System.out.println(peer.peerId+" "+peer.peerAddress+" "+peer.peerPort);
-			
+
 		}
 		System.out.println( p.myID);
 		System.out.println( p.haveFile);
@@ -94,50 +120,32 @@ public class peerProcess extends Thread {
 		System.out.println (p.fileSize);
 		System.out.println (p.pieceSize);
 		System.out.println (p.nofPieces);
-		*/
-		  new peerProcess();
+		 */
+		new peerProcess();
 	}
-	
-	   public peerProcess() throws Exception {
-		     server = new ServerSocket(3000);
-		     System.out.println("Server listening on port 3000.");
-		     this.start();
-		   }
+}
 
-		   public void run() {
-		     while(true) {
-		       try {
-		        System.out.println("Waiting for connections.");
-		        Socket client = server.accept();
-		        System.out.println("Accepted a connection from: "+
-		client.getInetAddress());
-		        Connect c = new Connect(client);
-		       } catch(Exception e) {}
-		     }
-		   }
+class Connect extends Thread {
+	private Socket client = null;
+	private ObjectInputStream ois = null;
+	private ObjectOutputStream oos = null;
+
+	public Connect() {}
+
+	public Connect(Socket clientSocket) {
+		client = clientSocket;
+		try {
+			ois = new ObjectInputStream(client.getInputStream());
+			oos = new ObjectOutputStream(client.getOutputStream());
+		} catch(Exception e1) {
+			try {
+				client.close();
+			}catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+			return;
 		}
+	}	
 
-		class Connect extends Thread {
-		   private Socket client = null;
-		   private ObjectInputStream ois = null;
-		   private ObjectOutputStream oos = null;
-
-		   public Connect() {}
-
-		   public Connect(Socket clientSocket) {
-		     client = clientSocket;
-		     try {
-		      ois = new ObjectInputStream(client.getInputStream());
-		      oos = new ObjectOutputStream(client.getOutputStream());
-		     } catch(Exception e1) {
-		         try {
-		            client.close();
-		         }catch(Exception e) {
-		           System.out.println(e.getMessage());
-		         }
-		         return;
-		     }
-		   }	
-	
 }
 
