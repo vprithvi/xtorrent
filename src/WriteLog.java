cc9b3c093b8c980674f5cd4ef2c89d0a28f0b461
Prithvi Raj <prithvi.raj@ufl.edu>
2011-11-24 15:16:37 -0500

    Added file logger

---
+++ src/WriteLog.java
@@ -0,0 +1,30 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+class WriteLog{
+	FileWriter outFile;
+	PrintWriter out;
+	String client_id;
+	boolean isReader;
+
+	public	WriteLog(String client_id){
+		this.client_id=client_id;
+		try {
+			outFile = new FileWriter(client_id+".log");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		out = new PrintWriter(outFile);
+	}
+
+
+	public void println(String msg){
+		out.flush();
+		out.println(msg);
+	}
+	public void close(){
+		out.println(" ");
+		out.close();}
+
+}
