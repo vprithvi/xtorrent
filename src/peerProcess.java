1989bc80758e4fe888431f864e2983d4777cf9e4
Prithvi Raj <prithvi.raj@ufl.edu>
2011-11-25 23:19:34 -0500

    Added oos/ois in contditionals in Connect code

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -170,44 +170,68 @@ class Connect extends Thread {
 		//		client = clientSocket;
 		server = serverSocket;
 		isServer = true;
+//		try {
+//			socket = server.accept();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
 		this.start();
 	}
 	
 	public Connect(String host, int port){
 	_host = host;
 	_port = port;
+//				try {
+//					socket = new Socket(_host,_port);
+//				} catch (UnknownHostException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
 	this.start();
 	}
 	public void run() {
 		try {
 			if(isServer) {
 				socket = server.accept(); 
-			} else {
-				socket = new Socket(_host,_port);
-			}
-			peerProcess.logger.println("Server: Accepted connection isServer"+isServer);
-
-			try {
+				peerProcess.logger.println("Server Accept crossed lp:"+ socket.getLocalPort()+" p"+socket.getPort());
 				ois = new ObjectInputStream(socket.getInputStream());
 				oos = new ObjectOutputStream(socket.getOutputStream());
+			} else {
+				socket = new Socket(_host,_port);
+				peerProcess.logger.println("New Server crossed lp:"+ socket.getLocalPort()+" p"+socket.getPort());
+				oos = new ObjectOutputStream(socket.getOutputStream());
+				ois = new ObjectInputStream(socket.getInputStream());
+			}
+			peerProcess.logger.println("Server: Accepted connection isServer:"+isServer);
+
+			try {
+				oos = new ObjectOutputStream(socket.getOutputStream());
+				ois = new ObjectInputStream(socket.getInputStream());
 			} catch(Exception e1) {
 				try {
-					socket.close();
+//					socket.close();
 				}catch(Exception e) {
 					peerProcess.logger.println(e.getMessage());
 				}
-				return;
+//				return;
 			}
-
-			oos.writeObject("Test String from "+peerProcess.myID+" and port "+peerProcess.myPort);
-			oos.flush();
-			
-			peerProcess.logger.print("Got a message !"+(String) ois.readObject()+"||||INFO: LocalPort "+socket.getLocalPort()+" RemotePort: "+socket.getPort());
-			
+            
+			if(isServer){
+				peerProcess.logger.print("Sent Message");
+				oos.writeObject("Test String from "+peerProcess.myID+" and port "+peerProcess.myPort);
+				oos.flush();
+			} else{
+				peerProcess.logger.print("Waiting for Message");
+				peerProcess.logger.print("Got a message !"+(String) ois.readObject()+"||||INFO: LocalPort "+socket.getLocalPort()+" RemotePort: "+socket.getPort());
+			}
 			// close streams and connections
-						ois.close();
-						oos.close();
-						socket.close(); 
+//						ois.close();
+//						oos.close();
+//						socket.close(); 
 		} catch(Exception e) {
 			peerProcess.logger.println(e.getMessage());
 
