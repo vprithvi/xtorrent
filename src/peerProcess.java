2b657b0ced17d4317863e0c63ac773df3e884c08
Prithvi Raj <prithvi.raj@ufl.edu>
2011-11-25 22:36:13 -0500

    Consolidated client and server classes

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -112,7 +112,7 @@ public class peerProcess extends Thread {
 								+ myPeerInfo.elementAt(x).peerAddress + " "
 								+ Integer.parseInt(myPeerInfo.elementAt(x).peerPort));
 						//					Socket socket = new Socket(myPeerInfo.elementAt(x).peerAddress,Integer.parseInt(myPeerInfo.elementAt(x).peerPort));
-						Client clientthread = new Client(myPeerInfo.elementAt(x).peerAddress,
+						Connect clientthread = new Connect(myPeerInfo.elementAt(x).peerAddress,
 								Integer.parseInt(myPeerInfo.elementAt(x).peerPort));
 						logger.println("crossed client" + myPeerInfo.elementAt(x).peerAddress
 								+ " on " + myPeerInfo.elementAt(x).peerPort);
@@ -155,11 +155,13 @@ public class peerProcess extends Thread {
 
 class Connect extends Thread {
 	//Socket client = server.accept();
-	private Socket client = null;
+	private Socket socket = null;
 	private ServerSocket server = null;
 	private ObjectInputStream ois = null;
 	private ObjectOutputStream oos = null;
-
+	String _host = null;
+	int _port = 0;
+	boolean isServer = false;
 	public Connect() {}
 
 	public Connect(ServerSocket serverSocket) {
@@ -167,32 +169,45 @@ class Connect extends Thread {
 		//						client.getInetAddress());
 		//		client = clientSocket;
 		server = serverSocket;
+		isServer = true;
 		this.start();
-	}	
+	}
+	
+	public Connect(String host, int port){
+	_host = host;
+	_port = port;
+	this.start();
+	}
 	public void run() {
 		try {
-			client = server.accept();
-			peerProcess.logger.println("Server: Accepted connection");
+			if(isServer) {
+				socket = server.accept(); 
+			} else {
+				socket = new Socket(_host,_port);
+			}
+			peerProcess.logger.println("Server: Accepted connection isServer"+isServer);
 
 			try {
-				ois = new ObjectInputStream(client.getInputStream());
-				oos = new ObjectOutputStream(client.getOutputStream());
+				ois = new ObjectInputStream(socket.getInputStream());
+				oos = new ObjectOutputStream(socket.getOutputStream());
 			} catch(Exception e1) {
 				try {
-					//					client.close();
+					socket.close();
 				}catch(Exception e) {
 					peerProcess.logger.println(e.getMessage());
 				}
 				return;
 			}
 
-
 			oos.writeObject("Test String from "+peerProcess.myID+" and port "+peerProcess.myPort);
 			oos.flush();
+			
+			peerProcess.logger.print("Got a message !"+(String) ois.readObject()+"||||INFO: LocalPort "+socket.getLocalPort()+" RemotePort: "+socket.getPort());
+			
 			// close streams and connections
-			//			ois.close();
-			//			oos.close();
-			//			client.close(); 
+						ois.close();
+						oos.close();
+						socket.close(); 
 		} catch(Exception e) {
 			peerProcess.logger.println(e.getMessage());
 
