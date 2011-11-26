65fb32b9deea1a485264059a625d94e45fcac190
Valampuri Lakshminarayanan <coolvalam@gmail.com>
2011-11-26 15:26:02 -0500

    COnstructor for actualmessage class.

--- src/ActualMessage.java
+++ src/ActualMessage.java
@@ -1,4 +1,15 @@
 
 public class ActualMessage {
+	
+	int length;
+	Byte messageType;
+	Byte messagePayload;
+	
+	ActualMessage(int l,Byte mT, Byte mP) {
+		length = l;
+		messageType = new Byte(mT); 
+		messagePayload = new Byte(mP); // Single chunk in byte
+		
+	}
 
 }
