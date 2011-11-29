eccb3e96ad108e216b7ff67f79ccee5c8df1889a
Prithvi Raj <prithvi.raj@ufl.edu>
2011-11-28 20:55:00 -0500

    Remove Main()

--- src/ActualMessage.java
+++ src/ActualMessage.java
@@ -3,14 +3,14 @@ import java.util.BitSet;
 
 
 public class ActualMessage implements Serializable {
-	
+
 
 	private static final long serialVersionUID = 3545119718998974040L;
 	int length;
 	int messageType;
 	byte[] messagePayload;
-	
-		
+
+
 	//Constructor for bitfield message
 	ActualMessage(int pieces) {
 		messageType = 5;
@@ -19,7 +19,7 @@ public class ActualMessage implements Serializable {
 		messagePayload = toByteArray(bitfield);
 		length = messagePayload.length;
 	}
-	
+
 	//Constructor for messages with chunk
 	ActualMessage(byte[] myChunk) {
 
@@ -27,19 +27,19 @@ public class ActualMessage implements Serializable {
 		messagePayload = myChunk.clone();
 
 		length = messagePayload.length;
-//		peerProcess.logger.println("Actual message payload content is \n\n\n"+new String(messagePayload)+"\n\n");
+		//		peerProcess.logger.println("Actual message payload content is \n\n\n"+new String(messagePayload)+"\n\n");
 	}
-	
 
-	
-/*	ActualMessage(int l,Byte mT, Byte mP) {
+
+
+	/*	ActualMessage(int l,Byte mT, Byte mP) {
 		length = l;
 		messageType = new Byte(mT); 
 		messagePayload = new Byte(mP); // Single chunk in byte
 		bitfield = new BitSet(l);
 		bitfield.set(1,true);
 	}*/
-	
+
 	public BitSet toBitSet(byte[] bytes) {
 		BitSet bits = new BitSet();
 		for(int i=0;i<bytes.length*8;i++) {
@@ -49,7 +49,7 @@ public class ActualMessage implements Serializable {
 		}
 		return bits;
 	}
-	
+
 	public byte[] toByteArray (BitSet bits) {
 		byte[] bytes = new byte[bits.length()/8+1];
 		for(int i=0;i<bits.length();i++) {
@@ -59,31 +59,7 @@ public class ActualMessage implements Serializable {
 		}
 		return bytes;
 	}
-	
-	public static void main(String arg[])
-	{
-		ActualMessage a = new ActualMessage(306);
-		System.out.println(a.length);
-		//System.out.println(a.messagePayload);
-		/*
-		BitSet mb = new BitSet(10);
-		mb.set(2);
-		System.out.println(mb.get(2));
-		System.out.println(mb.get(9));
-		byte[] mby = new byte[mb.length()/8+1];
-		mby = toByteArray(mb);
-		for (int i =0;i<mby.length;i++){
-		System.out.println(mby[i]);}
-		
-		BitSet mb2 = new BitSet();
-		mb2 = toBitSet(mby);
-		System.out.println(mb2.get(2));
-		System.out.println(mb2.get(9));
-		//System.out.println(a.messageType);
-		//System.out.println(a.bitfield.toString());
-		//System.out.println(a.bitfield.get(0));
-		 * */
-		 
-	}
+
+
 
 }
