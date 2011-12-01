e09c0ebf99b1ed32914f1ec4c0d9d57acab0aa69
Prithvi Raj <prithvi.raj@ufl.edu>
2011-12-01 18:18:52 -0500

    modified run

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 rm *.log
 pkill java
 cd bin
+rm -r {1..6}/*
 rm *.log
 cp ../*.cfg .
 java -ea StartRemotePeers
 sleep 3
 cat *.log
-grep -i connect *.log
+grep -B 15 -i connect.java *.log
+ls {1..6}
